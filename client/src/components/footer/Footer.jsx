b5ad7a0288cd0bb5ccd86bcfc923b4688e696a1c
Ppiinnkkk <quan05022001@gmail.com>
2022-06-07 09:15:19 +0700

    first commit

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="fLists">
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">hotels</li>
+                </ul>            
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">hotels</li>
+                </ul>           
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">hotels</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">hotels</li>
+                </ul>            
+                <ul className="fList">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">hotels</li>
+                </ul>
+            </div>   
+            <div className="fText">Copyright @ 2022 Lamabooking.</div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import "./navbar.css"
+import {Link, useNavigate} from "react-router-dom"
+import { useContext } from "react"
+import { AuthContext } from "../../context/AuthContext"
+
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const {user} = useContext(AuthContext);
+    const handleClick = () => {
+        navigate("/login");
+    }
+    return(
+        <div className="navbar">
+            <div className="navContainer">
+                <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
+                <span className="logo">QNL Booking</span>
+                </Link>
+                {user ? user.username : (
+                    <div className="navItem">
+                        <button className="navButton">Register</button>
+                        <button onClick={handleClick} className="navButton">Login</button>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
