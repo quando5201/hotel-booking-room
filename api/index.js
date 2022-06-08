e9d29f346c0e7ff42915eb0cb5a39c3bc85b1977
Ppiinnkkk <73686006+Ppiinnkkk@users.noreply.github.com>
2022-06-09 00:12:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.use("/api/rooms", roomsRoute);
 
 app.use((err,req,res,next)=>{
   const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something wnet wrong!"
+  const errorMessage = err.message || "Something went wrong!"
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
